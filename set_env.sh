a778b5a95b0a25620bef77ffe3dbf1c50f2429b4
thachvu2410 <quangthach31@gmail.com>
2023-10-17 21:29:06 +0700

    update workflow

--- set_env.sh
+++ set_env.sh
@@ -2,8 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=admin123
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
+export POSTGRES_HOST=postgres.cxsu8o3dnwsn.us-east-1.rds.amazonaws.com
 export AWS_BUCKET=arn:aws:s3:::mybucket2410
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
