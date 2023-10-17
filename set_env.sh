c521b1a784e4553ff2d77e4636ab36bfe2db8ab3
thachvu2410 <quangthach31@gmail.com>
2023-10-17 21:46:59 +0700

    upload bucketName

--- set_env.sh
+++ set_env.sh
@@ -3,7 +3,7 @@
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=admin123
 export POSTGRES_HOST=postgres.cxsu8o3dnwsn.us-east-1.rds.amazonaws.com
-export AWS_BUCKET=arn:aws:s3:::mybucket2410
+export AWS_BUCKET=mybucket2410
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
