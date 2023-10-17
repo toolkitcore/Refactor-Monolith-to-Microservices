12761a7b5ef35a365f099c84a2b057a0e5dcda7d
thachvu2410 <quangthach31@gmail.com>
2023-10-17 22:56:19 +0700

    update apiHost

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab20ef7d76893442cb797762483b41d5-1326552066.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
